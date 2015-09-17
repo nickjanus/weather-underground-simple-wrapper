695144f5e2229945a895d853dcc349b8ffde126e
benrhere <i0b1mrcs@hotmail.com>
2015-09-17 10:51:33 -0400

    Added the modules to the repository

---
+++ service.py
@@ -0,0 +1,15 @@
+﻿import json
+import urllib.request
+
+
+def call_restful_service(service_url):
+    """Generic method to call a web service over HTTP and return the response as JSON
+
+    Keyword arguments:
+    service_url -- a string containing the URL to call. Note that we expect all variables
+                   to have been filled in already and expect a URL to call (no replacements)
+    """
+    _response = urllib.request.urlopen(service_url)
+    _parsed_json = json.loads(_response.read().decode('utf-8'))
+    _response.close()
+    return _parsed_json
